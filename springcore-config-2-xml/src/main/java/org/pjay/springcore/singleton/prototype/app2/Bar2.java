7111852121e224785720b7c2ed1674e86b22cd61
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-08-21 16:52:05 +0530

    Vijay - 2017/08/21 - Spring core examples

---
+++ springcore-config-2-xml/src/main/java/org/pjay/springcore/singleton/prototype/app2/Bar2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.pjay.springcore.singleton.prototype.app2;
+
+/**
+ * @author Vijay
+ * 
+ */
+public class Bar2 {
+
+	private String abc;
+
+	public String getAbc() {
+		return abc;
+	}
+
+	public void setAbc(String abc) {
+		this.abc = abc;
+	}
+
+	@Override
+	public String toString() {
+		return "Bar2 [abc=" + abc + "]";
+	}
+}
---
+++ springcore-config-2-xml/src/main/java/org/pjay/springcore/singleton/prototype/app2/Foo2.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.pjay.springcore.singleton.prototype.app2;
+
+/**
+ * @author Vijay
+ * 
+ */
+public abstract class Foo2 {
+	public abstract Bar2 buildBean();
+}
