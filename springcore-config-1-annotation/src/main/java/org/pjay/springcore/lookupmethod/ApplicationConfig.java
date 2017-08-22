87a695b1160db3b8506abff38ce210b9051f3d4b
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-08-22 19:36:25 +0530

    Vijay - 2017/08/22 - Spring core examples

---
+++ springcore-config-1-annotation/src/main/java/org/pjay/springcore/lookupmethod/ApplicationConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.pjay.springcore.lookupmethod;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author vijayk
+ *
+ */
+@Configuration
+@ComponentScan(basePackages= {"org.pjay.springcore.lookupmethod"})
+//@ComponentScan(basePackages= "org.pjay.springcore.lookupmethod")
+public class ApplicationConfig {
+	
+}
