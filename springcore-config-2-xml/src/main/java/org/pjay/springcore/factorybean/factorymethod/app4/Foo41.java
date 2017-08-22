c205fc18f9bbc5ab2d3cd948f7901985972fe8e1
Vijay Konduru <konduruvijaykumar@gmail.com>
2017-08-22 10:09:49 +0530

    Vijay - 2017/08/22 - Spring core examples

---
+++ springcore-config-2-xml/src/main/java/org/pjay/springcore/factorybean/factorymethod/app4/Foo41.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.springcore.factorybean.factorymethod.app4;
+
+/**
+ * @author vijayk
+ *
+ */
+public class Foo41 implements Foo4 {
+
+	@Override
+	public void print() {
+		System.out.println(" :: print from Foo41 :: ");
+	}
+
+}
---
+++ springcore-config-2-xml/src/main/java/org/pjay/springcore/factorybean/factorymethod/app4/Foo42.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.pjay.springcore.factorybean.factorymethod.app4;
+
+/**
+ * @author vijayk
+ *
+ */
+public class Foo42 implements Foo4 {
+
+	@Override
+	public void print() {
+		System.out.println(" :: print from Foo42 :: ");
+	}
+
+}
